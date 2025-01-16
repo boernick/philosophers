4e9b3722a90e0489edd42cbf5d653b9ce155d8df
Nick Boer <nboer@c1b4c3.42berlin.de>
2025-01-16 16:31:51 +0100

    update

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nick <nick@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: nboer <nboer@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 14:26:16 by nboer             #+#    #+#             */
-/*   Updated: 2025/01/13 23:01:33 by nick             ###   ########.fr       */
+/*   Updated: 2025/01/16 16:31:43 by nboer            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@ int	put_error(char *msg)
 	return (EXIT_FAILURE);
 }
 
-int	init_philos(t_rules *rules)
+int	init_philos(t_rules *rules, t_philo *philo)
 {
 	int	i;
 	
 	i = 0;
 	while (i < rules->philos_n)
 	{
-		philos[i].time_death = i;
-		philos[i].time_sleep = 0;
-		philos[i].time_eat = 0;
+		philo[i].t_death = i;
+		philo[i].t_sleep = 0;
+		philo[i].t_eat = 0;
 		i++;
 	}
 }
@@ -40,19 +40,22 @@ void	start_eating(t_philo *philo)
 int init_diner(t_rules *rules, char **argv)
 {
 	set_rules(rules, argv);
-	if (check_input(rules) == 1)
+	if (wrong_input(rules, argv))
 		return (put_error("wrong input"));
 	
 
 }
 
-int	check_input(t_rules *rules, char **argv))
+int	wrong_input(t_rules *rules, char **argv)
 {
 	if (rules->philos_n <= 1 || rules->philos_n > 100 || rules->t_eat < 0 
 		|| rules->t_sleep < 0 || rules->t_death < 0)
 		return (EXIT_FAILURE);
-	
-	
+	if (argv[5])
+	{
+		if (rules->n_meals < 1)
+			return (EXIT_FAILURE);
+	}
 }
 
 void	set_rules(t_rules *rules, char **argv)
@@ -64,21 +67,19 @@ void	set_rules(t_rules *rules, char **argv)
 	rules->philos_dead = 0;
 	rules->philos_fed = 0;
 	if (argv[5])
-	{
 		rules->n_meals = ft_atoi(argv[5]);
-	}
+	else 
+		rules->n_meals = -1;
 }
 
 int	main(int argc, char **argv)
 {
 	t_rules rules;
+	t_philo philo;
 	
 	set_rules(&rules, argv);
 	if (argc != 5 && argc != 6)
-	{
-		ft_putendl_fd("Error: wrong argument count", STDERR_FILENO);
-		return (1);
-	}
-	//init_philos(rules);
+		return (put_error("Error: wrong argument count"));
+	init_philos(&rules, &philo);
 	//init_diner;
 }
